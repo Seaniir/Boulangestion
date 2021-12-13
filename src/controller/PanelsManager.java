68ee43c0ec24029b7101c427373210f7b5b2df56
Julien <julien.legros91@gmail.com>
2021-12-13 14:43:11 +0100

    Login Fix pas dans le HUB

--- src/controller/PanelsManager.java
+++ src/controller/PanelsManager.java
@@ -1,5 +1,6 @@
 package controller;
 
+import view.Login;
 import view.lambdaPanel;
 
 import javax.swing.*;
@@ -7,4 +8,5 @@ import javax.swing.*;
 public class PanelsManager {
     public static JPanel contentPane;
     public static JPanel switchToLambdaPanel(){return new lambdaPanel();}
+    public static JPanel switchToLoginPanel(){return new Login();}
 }
