cebe3d02cac87e69674bf83028aab5055cbfecd7
Fburbaud <fred.brechet@gmail.com>
2021-12-13 16:56:39 +0100

    Créer le swing de la page Fournisseurs(wip)

--- src/controller/PanelsManager.java
+++ src/controller/PanelsManager.java
@@ -1,6 +1,8 @@
 package controller;
 
 import view.AccueilMenu;
+import view.ListeFournisseurs;
+import view.NewFournisseur;
 import view.lambdaPanel;
 
 import javax.swing.*;
@@ -10,5 +12,6 @@ public class PanelsManager {
     public static JPanel switchToLambdaPanel(){return new lambdaPanel();}
     public static JPanel switchToAccueilMenu(){return new AccueilMenu();}
     //public static JPanel switchToCommandesClientView(){return new CommandesClientView();}
-    
+    public static JPanel switchToListeFournisseurs(){return new ListeFournisseurs();}
+    public static JPanel switchToNewFournisseur(){return new NewFournisseur();}
 }
--- src/view/AccueilMenu.java
+++ src/view/AccueilMenu.java
@@ -319,7 +319,7 @@ public class AccueilMenu extends JPanel {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				PanelsManager.contentPane.removeAll();
-				PanelsManager.contentPane.add(PanelsManager.switchToLambdaPanel());
+				PanelsManager.contentPane.add(PanelsManager.switchToNewFournisseur());
 				PanelsManager.contentPane.repaint();
 				PanelsManager.contentPane.revalidate();
 			}
