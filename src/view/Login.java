31a9d4d982ba928d50aeb037c00ea968b94e83dc
Quentin <wanheyda91@gmail.com>
2021-12-21 10:42:52 +0100

    Fix

--- src/view/Login.java
+++ src/view/Login.java
@@ -40,7 +40,7 @@ public class Login extends JPanel {
 		JLabel lblBoulangestion = new JLabel("Boulangestion");
 		lblBoulangestion.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBoulangestion.setForeground(new Color(157, 70, 6));
-		lblBoulangestion.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 80));
+		lblBoulangestion.setFont(new Font("Gentium Book Basic", Font.BOLD | Font.ITALIC, 80));
 		lblBoulangestion.setBounds(10, 38, 1420, 98);
 		add(lblBoulangestion);
 		
